3696960d5af5c0324a8f00d88ea18d2550332983
Devendra Uraon <devendra123world@gmail.com>
2022-04-09 00:44:57 +0530

    Update Solution_Minion_Task_Schedule.py

--- Level-1/Solution_Minion_Task_Schedule.py
+++ Level-1/Solution_Minion_Task_Schedule.py
@@ -1,17 +1,17 @@
 #Minion Task Schedule
 
 def solution(data, n):
-    new = []
+    ans = []
     if len(data) < 100:
         for d in data:
             if n <= 1:
                 if data.count(d) > n:
                     pass
                 elif data.count(d) <= n:
-                    new.append(d)
+                    ans.append(d)
             elif n > 1:
                 if data.count(d) > n:
                     pass
                 elif data.count(d) <= n:
-                    new.append(d)
-    return new
+                    ans.append(d)
+    return ans
